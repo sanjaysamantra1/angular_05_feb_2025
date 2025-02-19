01626ed0690fb4fe1e40053b7ec25e6ac70ae909
budhinirao <budhiniamaraneni@gmail.com>
2025-02-19 15:15:34 -0500

    directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DirectivesComponent } from './Components/directives/directives.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    DirectivesComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular1';
