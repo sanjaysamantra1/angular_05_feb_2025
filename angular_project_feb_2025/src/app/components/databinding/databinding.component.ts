cc58ac8ccd6586f48f98b5c175dbfb80c9271c78
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-02-19 07:06:57 +0530

    css

--- angular_project_feb_2025/src/app/components/databinding/databinding.component.ts
+++ angular_project_feb_2025/src/app/components/databinding/databinding.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -7,17 +7,28 @@ import { FormsModule } from '@angular/forms';
     FormsModule
   ],
   templateUrl: './databinding.component.html',
-  styleUrl: './databinding.component.css'
+  // styles: ['input { color:green; }']
+  styleUrl: './databinding.component.css',
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class DatabindingComponent {
   myName: string = 'Virat Kohli';
   profile_url: string = 'https://documents.bcci.tv/resizedimageskirti/164_compress.png';
   flag: boolean = false;
+  msg: string = '';
 
   num1: number = 10;
   num2: number = 20;
 
+  colspanValue: number = 2;
+  user = { fName: 'Sachin', lName: 'Tendulkar', email: 'sachin@gmail.com', age: 45 };
+
   sayHello() {
     alert('Helllooooooo')
+  }
+
+  additionResult: number = 0;
+  addition(a: string, b: string) {
+    this.additionResult = +a + +b;
   }
 }
