c5ec5f37c395d4bdbebf07dc6c7d4fedc6d2ebe7
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-03-05 21:33:18 +0530

    observable

--- angular_project_feb_2025/src/app/components/observable-demo3/observable-demo3.component.ts
+++ angular_project_feb_2025/src/app/components/observable-demo3/observable-demo3.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-observable-demo3',
@@ -7,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './observable-demo3.component.css'
 })
 export class ObservableDemo3Component {
+  githubData: any[] = []
 
+  constructor(private httpClient: HttpClient) {
+  }
+  ngOnInit() {
+    let sanjayDataProvider = this.httpClient.get('https://jsonplaceholder.typicode.com/users/1');
+    let remanaDataProvider = this.httpClient.get('https://jsonplaceholder.typicode.com/users/3');
+    let akshitaDataProvider = this.httpClient.get('https://jsonplaceholder.typicode.com/users/9');
+
+    forkJoin([sanjayDataProvider, remanaDataProvider, akshitaDataProvider]).subscribe(
+      ([sanjayData, remanaData, akshitaData]) => {
+        this.githubData = [sanjayData, remanaData, akshitaData];
+        console.log(this.githubData);
+      }
+    );
+  }
 }
